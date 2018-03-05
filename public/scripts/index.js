461a7d8d016ac0d3f14d3a5d22e8a6ed5d85e1b8
Nick Nunez <nickn10@comcast.net>
2018-03-05 15:23:35 -0600

    Add play/pause button (non-functional)

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,5 @@
 const sequencer = document.getElementById('sequencer');
-
+const playBtn = document.getElementById('play-btn');
 
 window.addEventListener('keydown', (e) => {
    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -8,13 +8,26 @@ window.addEventListener('keydown', (e) => {
    audio.play();
 })
 
-sequencer.addEventListener('click', (e) => {
+sequencer.addEventListener('mouseup', (e) => {
   if(e.target.classList.contains('beat-pad')){
      e.target.classList.toggle('active');
   }
+});
+
+sequencer.addEventListener('mousedown', (e) => {
   if(e.target.tagName === 'BUTTON') {
     const audio = e.target.previousElementSibling;
     audio.currentTime = 0;
     audio.play();
   }
 });
+
+playBtn.addEventListener('click', () => {
+  if(playBtn.classList.contains('playing')) {
+    playBtn.innerHTML = '<i class="material-icons">play_arrow</i>'
+  } else {
+    playBtn.innerHTML = '<i class="material-icons">pause</i>'
+  }
+  playBtn.classList.toggle('playing');
+})
+
