2e7d729940ca61e23b70e0e003ad792622c3d0bb
Nick Nunez <nickn10@comcast.net>
2018-03-05 14:26:49 -0600

    Remove Material Kit, update sequencer design and layout.  Add click event to instrument buttons.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,13 +1,20 @@
 const sequencer = document.getElementById('sequencer');
 
+
 window.addEventListener('keydown', (e) => {
    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
    if(!audio) return;
    audio.currentTime = 0;
    audio.play();
 })
+
 sequencer.addEventListener('click', (e) => {
   if(e.target.classList.contains('beat-pad')){
      e.target.classList.toggle('active');
-  };
+  }
+  if(e.target.tagName === 'BUTTON') {
+    const audio = e.target.previousElementSibling;
+    audio.currentTime = 0;
+    audio.play();
+  }
 });
