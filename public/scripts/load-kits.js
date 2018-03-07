349655ad19e3afe94e55ef8a10f2a55f40ab5016
Nick Nunez <nickn10@comcast.net>
2018-03-07 09:45:31 -0600

    Fix conflict in load-kits.js

--- public/scripts/load-kits.js
+++ public/scripts/load-kits.js
@@ -6,21 +6,12 @@ const LoadKits = (function(){
          instruments: ['KICK-1', 'KICK-2', 'SNARE-1', 'SNARE-2', 'HHAT-1','HHAT-2','TOM-1','TOM-2','RIDE','CRASH', 'TAMBORINE', 'SHAKER']
       },
       hipHop: {
-<<<<<<< HEAD
          path: 'https://raw.githubusercontent.com/nickn10/beat-sequencer/master/assets/audio/hip-hop/',
-         instruments: ['KICK-1', 'KICK-2', 'SNARE-1', 'SNARE-2', 'HHAT-1', 'HHAT-2','CLAP-1', 'PERC-1','PERC-2','PERC-3', 'CYMBAL-1', 'VOCAL-1', 'VOCAL-2', 'VOCAL-3', 'VOCAL-4']
+         instruments: ['KICK-1', 'KICK-2', 'SNARE-1', 'HHAT-1', 'HHAT-2', 'CLAP-1', 'PERC-1', 'PERC-2', 'PERC-3', 'CYMBAL-1', 'VOCAL-1']
       },
       house: {
          path: 'https://raw.githubusercontent.com/nickn10/beat-sequencer/master/assets/audio/house/',
-         instruments: ['KICK-1', 'KICK-2', 'SNARE-1', 'CLAP-1','CLAP-2', 'HHAT-1', 'HHAT-2', 'PERC-1', 'PERC-2', 'CYMBAL-1', 'CYMBAL-2','STAB-1', 'STAB-2', 'TOM-1', 'TOM-2']
-=======
-         path: 'assets/audio/hip-hop/',
-         instruments: ['KICK-1', 'KICK-2', 'SNARE-1', 'HHAT-1', 'HHAT-2','CLAP-1', 'PERC-1','PERC-2','PERC-3', 'CYMBAL-1','VOCAL-1']
-      },
-      house: {
-         path: 'assets/audio/house/',
-         instruments: ['KICK-1', 'SNARE-1', 'CLAP-1','CLAP-2', 'HHAT-1', 'HHAT-2', 'PERC-1', 'PERC-2', 'CYMBAL-1','STAB-1', 'TOM-1']
->>>>>>> dev
+         instruments: ['KICK-1', 'SNARE-1', 'CLAP-1', 'CLAP-2', 'HHAT-1', 'HHAT-2', 'PERC-1', 'PERC-2', 'CYMBAL-1', 'STAB-1', 'TOM-1']
       },
       techno: {
          path: 'https://raw.githubusercontent.com/nickn10/beat-sequencer/master/assets/audio/techno/',
