813d8b25b40f561db02db1b3803dbd6e5922db8d
Nick Nunez <nickn10@comcast.net>
2018-03-08 22:29:26 -0600

    Update kits to correct path for gh-pages.

--- public/scripts/kits.js
+++ public/scripts/kits.js
@@ -1,18 +1,18 @@
 const Kits = (function() {
    return ({
       rock: {
-         // path: 'https://raw.githubusercontent.com/nickn10/beat-sequencer/master/assets/audio/rock/',
-         path: 'assets/audio/rock/',
+         path: 'https://raw.githubusercontent.com/nickn10/beat-sequencer/master/assets/audio/rock/',
+         // path: 'assets/audio/rock/',
          instruments: ['KICK-1', 'KICK-2', 'SNARE-1', 'HHAT-1', 'HHAT-2', 'TOM-1', 'TOM-2', 'RIDE', 'CRASH', 'TAMBORINE', 'SHAKER'],
       },
       hipHop: {
-         // path: 'https://raw.githubusercontent.com/nickn10/beat-sequencer/master/assets/audio/hip-hop/',
-         path: 'assets/audio/hip-hop/',
+         path: 'https://raw.githubusercontent.com/nickn10/beat-sequencer/master/assets/audio/hip-hop/',
+         // path: 'assets/audio/hip-hop/',
          instruments: ['KICK-1', 'KICK-2', 'SNARE-1', 'HHAT-1', 'HHAT-2', 'CLAP-1', 'PERC-1', 'PERC-2', 'PERC-3', 'CYMBAL-1', 'VOCAL-1'],
       },
       house: {
-         // path: 'https://raw.githubusercontent.com/nickn10/beat-sequencer/master/assets/audio/house/',
-         path: 'assets/audio/house/',
+         path: 'https://raw.githubusercontent.com/nickn10/beat-sequencer/master/assets/audio/house/',
+         // path: 'assets/audio/house/',
          instruments: ['KICK-1', 'SNARE-1', 'CLAP-1', 'CLAP-2', 'HHAT-1', 'HHAT-2', 'PERC-1', 'PERC-2', 'CYMBAL-1', 'STAB-1', 'TOM-1'],
       },
       techno: {
